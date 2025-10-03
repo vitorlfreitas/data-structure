e21fdc6d0baeb66c522746b1c9ef3ea051aa74d3
vitorlfreitas <vitor.lucfreitas@gmail.com>
2025-10-03 14:55:27 +0100

    Implements a java program that implements a DSU Union-Find with rank and path compression

--- DisjointSet/src/Main.java
+++ DisjointSet/src/Main.java
@@ -1,3 +1,4 @@
+import dsu_implementation.Dsu;
 import dsu_implementation.SimpleDsu;
 import dsu_implementation.UnionByRank;
 
@@ -8,15 +9,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SimpleDsu dsu = new SimpleDsu(5);
+        SimpleDsu sdsu = new SimpleDsu(5);
 
-        System.out.println(dsu.find(0) + " " + dsu.find(2));
+        System.out.println(sdsu.find(0) + " " + sdsu.find(2));
         // Output: 0 2
 
         // Union 2 subsets
-        dsu.union(0, 2);
+        sdsu.union(0, 2);
 
-        System.out.println(dsu.find(0) + " " + dsu.find(2));
+        System.out.println(sdsu.find(0) + " " + sdsu.find(2));
         // Output: 0 0
 
         System.out.println();
@@ -31,6 +32,45 @@ public class Main {
 
         System.out.println(ur.find(0) + " " + ur.find(2));
         // Output: 2 2
+
+        System.out.println();
+
+        // Declares a new instance of DSU
+        Dsu dsu = new Dsu(5);
+
+        // Prints parent initialized as each element as itw own distinct set
+        System.out.print("[ ");
+        for (int i : dsu.getParent()) {
+            System.out.print(i + ", ");
+        }
+        System.out.println("]");
+
+        System.out.println(dsu.find(0) + " " + dsu.find(2));
+        // Output: 0 2
+
+        // Union 2 subsets
+        dsu.union(0, 2);
+
+        System.out.println(dsu.find(0) + " " + dsu.find(2));
+        // Output: 0 0
+
+        System.out.print("[ ");
+        for (int i : dsu.getParent()) {
+            System.out.print(i + ", ");
+        }
+        System.out.println("]");
+
+        dsu.union(2, 4);
+        dsu.union(4, 3);
+
+        System.out.print("[ ");
+        for (int i : dsu.getParent()) {
+            System.out.print(i + ", ");
+        }
+        System.out.println("]");
+        // [ 2, 1, 2, 2, 2, ]
+        // Which is: [{2, 2, 2, 2},{1}]
+
 
 
     }
---
+++ DisjointSet/src/dsu_implementation/Dsu.java
@@ -0,0 +1,70 @@
+package dsu_implementation;
+
+/**
+ * Implementation of the Disjoint Set (Union-Find) Data Structure
+ * Using the both optimization, Union by rank, and Path Compression
+ */
+public class Dsu {
+
+    private final int[] parent;
+    private final int[] rank;
+
+    public Dsu(int size) {
+
+        // Initialize the array with length = size
+        parent = new int[size];
+        rank = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            // Define all set as its own subset
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    /**
+     * Find the root parent of x
+     * @param x the set to be found
+     * @return the parent of the node
+     */
+    public int find(int x) {
+
+        if (parent[x] == x)
+            return x;
+
+        // Path Compression: It makes the x as a child of parent root
+        parent[x] = find(parent[x]);
+
+        // return the parent of x
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+
+        int rootX = this.find(x);
+        int rootY = this.find(y);
+
+        // If rootX and rootY are from same set already, "DO NOTHING"
+        if (rootX == rootY)
+            return;
+
+        // Union by rank
+        // Attach the smaller rank tree under the root of the higher rank tree
+        if (rank[rootX] < rank[rootY])
+            parent[rootX] = rootY;
+
+        else if (rank[rootX] > rank[rootY])
+            parent[rootY] = rootX;
+            // If ranks are same, then make one as root of another and increment its rank by one
+        else {
+            parent[rootX] = rootY;
+            rank[rootY] += 1;
+        }
+    }
+
+    public int[] getParent() {
+        return parent;
+    }
+
+
+}
